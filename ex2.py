218580d5a35b5eb09c857a500ec40c40ee810de4
dogOfRiddles <gporhp@gmail.com>
2018-11-19 20:48:47 -0500

    A Consideration of Facts

---
+++ ex2.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+print('Exercise 2: The Way Things Are Different')
+
+print('Could you consider submitting a number?')
+whatTheUserDecides = input()
+aDecidedNumber = int(whatTheUserDecides)
+
+if (aDecidedNumber % 3) == 0:
+    print('It\'s divisble by 3. How nice!')
+
+if (aDecidedNumber % 4) == 0:
+    print('It\'s divisible by 4. Nice digs.')
+
+if (aDecidedNumber % 12) == 0:
+    print('Wow, you\'re really testing this.')
+
+if (aDecidedNumber % 88) == 0:
+    print('Q is among us!')
+    r = (requests.get('https://api.whatdoestrumpthink.com/api/v1/quotes/random').json())
+    print(r['message'])
+    #may his message be mocked for all days
