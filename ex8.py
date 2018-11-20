489dfafbfcfed2e7ff8a0493dea3a75dd8596ba2
dogOfRiddles <gporhp@gmail.com>
2018-11-20 15:26:48 -0500

    RPS on Demand

--- ex8.py
+++ ex8.py
@@ -1,18 +1,18 @@
 import sys
 import dogUtil
 
-argVariable = 0
+if len(sys.argv) == 1 or len(sys.argv) == 2:
+    print('Consider using the command line argument \'python3 <script> <argument> <argument>\'')
+    print('Defaulting to "random"')
+    dogUtil.adjudicateChops()
 
-if len(sys.argv) == 1:
-    print('Consider using the command line argument \'python3 <script> <argument>\'')
-    print('Defaulting to "default value"')
-
-if len(sys.argv) == 2:
-    print('Received submission: ',sys.argv[1])
+if len(sys.argv) == 3:
+    print('Received submission: ',sys.argv[1],'and',sys.argv[2])
     try:
-        argVariable = sys.argv[1]
+        player1 = sys.argv[1]
+        player2 = sys.argv[2]
+        dogUtil.adjudicateChops(player1,player2)
     except:
         print('Invalid Input. Reverting to default.')
-        argVariable = 0
 
-dogUtil.makeUserConfirm('heliotrope')
+#dogUtil.makeUserConfirm('heliotrope')
