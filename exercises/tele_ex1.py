752437e365ee9d7e198bc4fbb7b0c549e9253c36
dogOfRiddles <gporhp@gmail.com>
2018-11-22 16:35:52 -0500

    self-info

--- exercises/tele_ex1.py
+++ exercises/tele_ex1.py
@@ -7,5 +7,9 @@ api_hash = '3cf434b3393e0db93d9e007c30fc2f23'
 
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
-client.send_message('me','This message brought to you by Python3')
+
+me = client.get_me()
+print(me.stringify())
+
+#client.send_message('me','This message brought to you by Python3')
 client.disconnect()
