80045d6324b04c09635870ed9d38437a30523283
dogOfRiddles <gporhp@gmail.com>
2018-11-24 23:27:17 -0500

    style guide

--- template.py
+++ template.py
@@ -3,13 +3,13 @@ import sys
 argVariable = 0
 
 if len(sys.argv) == 1:
-    print('Consider using the command line argument \'python3 <script> <argument>\'')
+    print('FYI: command line argument \'python3 <script> <argument>\'')
     print('Defaulting to "default value"')
 
 if len(sys.argv) == 2:
-    print('Received submission: ',sys.argv[1])
+    print('Received submission: ', sys.argv[1])
     try:
         argVariable = sys.argv[1]
-    except:
+    except Exception:
         print('Invalid Input. Reverting to default.')
         argVariable = 0
